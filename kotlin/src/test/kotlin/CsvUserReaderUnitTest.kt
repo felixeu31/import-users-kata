47af43b0cc56b767ed59849519f7e9db6b65acf9
Félix Díez <felixeu31@gmail.com>
2023-08-16 08:40:45 +0200

    test: add test coverage with should_return_parsed_user_from_csv_file

--- kotlin/src/test/kotlin/CsvUserReaderUnitTest.kt
+++ kotlin/src/test/kotlin/CsvUserReaderUnitTest.kt
@@ -21,4 +21,25 @@ class CsvUserReaderUnitTest {
         // Then
         assertEquals(true, users.isEmpty())
     }
+
+    @Test
+    fun should_return_parsed_user_from_csv_file() {
+        // Given
+        val fileStream: InputStream = """id,gender,name,country,postcode,email,birthdate
+200189617246,male,Lukas Schmidt,Germany,10780,lukas.shmidt@example.com,1997-02-19T04:10:00.000Z""".byteInputStream()
+        val fileReaderMock = mockk<FileReader>()
+        every { fileReaderMock.getFileAsInputStream("mockPath") } returns fileStream
+        val csvUserReader: CsvUserReader = CsvUserReader("mockPath", fileReaderMock)
+
+        // When
+        val users = csvUserReader.getUsers()
+
+        // Then
+        assertEquals(false, users.isEmpty())
+        assertEquals(1, users.size)
+        assertEquals(200189617246, users.first().id)
+        assertEquals("Germany", users.first().country)
+        assertEquals("lukas.shmidt@example.com", users.first().email)
+        assertEquals("Lukas Schmidt", users.first().name)
+    }
 }
