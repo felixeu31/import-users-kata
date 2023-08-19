9cb5430143d37ebb3a76383327cce6960239ad6c
Félix Díez <felixeu31@gmail.com>
2023-08-19 09:40:43 +0200

    refactor: simplify WebUserReader logic

--- kotlin/src/main/kotlin/WebUserReader.kt
+++ kotlin/src/main/kotlin/WebUserReader.kt
@@ -1,38 +1,23 @@
 import org.json.JSONObject
-import java.math.BigInteger
-import java.util.*
 
 class WebUserReader constructor(private val url: String, private val apiClient: ApiClient): UserReader {
     override fun getUsers(): List<User> {
-        val webUsers = ArrayList<Array<String>>()
+        val users = mutableListOf<User>()
         val usersResponse = apiClient.executeRequest(url)
+        var id: Long = 100000000000
 
-
-        val jsonObject = JSONObject(usersResponse)
-        val results = jsonObject.getJSONArray("results")
-        var j = BigInteger("100000000000")
+        val results = JSONObject(usersResponse).getJSONArray("results")
         for (i in 0 until results.length()) {
-            j = j.add(BigInteger("1"))
-            webUsers.add(
-                arrayOf(
-                    j.toString(),  // id
-                    results.getJSONObject(i).getString("gender"),
+            users.add(
+                User(
+                    ++id,
+                    results.getJSONObject(i).getString("email"),
                     results.getJSONObject(i).getJSONObject("name").getString("first") + " " + results.getJSONObject(i)
                         .getJSONObject("name").getString("last"),
-                    results.getJSONObject(i).getJSONObject("location").getString("country"),
-                    results.getJSONObject(i).getJSONObject("location")["postcode"].toString(),
-                    results.getJSONObject(i).getString("email"),
-                    GregorianCalendar()[Calendar.YEAR].toString()
+                    results.getJSONObject(i).getJSONObject("location").getString("country")
                 )
             )
         }
-        return webUsers.map { rawUser ->
-            User(
-                rawUser[0].toLong(),
-                rawUser[5],
-                rawUser[2],
-                rawUser[3],
-            )
-        }
+        return users
     }
 }
