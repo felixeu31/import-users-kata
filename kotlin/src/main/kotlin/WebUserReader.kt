8a8011839ac5d400beefbe09fc14aa2613c85f92
Félix Díez <felixeu31@gmail.com>
2023-08-19 09:51:13 +0200

    refactor: extract function on web get users

--- kotlin/src/main/kotlin/WebUserReader.kt
+++ kotlin/src/main/kotlin/WebUserReader.kt
@@ -1,12 +1,17 @@
+import org.json.JSONArray
 import org.json.JSONObject
 
 class WebUserReader constructor(private val url: String, private val apiClient: ApiClient): UserReader {
     override fun getUsers(): List<User> {
-        val users = mutableListOf<User>()
         val usersResponse = apiClient.executeRequest(url)
-        var id: Long = 100000000000
 
         val results = JSONObject(usersResponse).getJSONArray("results")
+        return parseJsonResponse(results)
+    }
+
+    private fun parseJsonResponse(results: JSONArray): List<User> {
+        val users = mutableListOf<User>()
+        var id: Long = 100000000000
         for (i in 0 until results.length()) {
             users.add(
                 User(
